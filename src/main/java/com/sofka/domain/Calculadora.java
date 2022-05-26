33f53beb99bb79a60526c298950830309dcead16
certhakzu <pealveco@yahoo.com>
2022-05-25 21:12:37 -0500

    agrego documentación

--- src/main/java/com/sofka/domain/Calculadora.java
+++ src/main/java/com/sofka/domain/Calculadora.java
@@ -1,44 +1,98 @@
 package com.sofka.domain;
 
+/**
+ * Esta clase define una abstracción de una Calculadora
+ * @autor: Piter Velasquez & Álvaro Vega
+ * @version 25/05/2022
+ */
 public class Calculadora {
     private double number1;
     private double number2;
 
+    /**
+     * Constructor por defecto
+     *
+     */
     public Calculadora(){}
 
+    /**
+     * Constructor con parámetrdos para la CLase Calculadora
+     * @param number1 primero operando
+     * @param number2 segundo operando
+     */
     public Calculadora(double number1, double number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
 
+    /**
+     * Método que regresa number1
+     * @return number1 primer operando
+     */
     public double getNumber1() {
         return number1;
     }
 
+    /**
+     * Método para asignar number1
+     * @param number1
+     */
     public void setNumber1(double number1) {
         this.number1 = number1;
     }
 
+    /**
+     * Método que regresa number2
+     * @return number2 segundo operando
+     */
     public double getNumber2() {
         return number2;
     }
 
+    /**
+     * Método para asignar number2
+     * @param number2
+     */
     public void setNumber2(double number2) {
         this.number2 = number2;
     }
 
+    /**
+     * Método para sumar
+     * @param number1 primer operando
+     * @param number2 segundo operando
+     * @return el Resultado de la SUMA
+     */
     public double add(double number1, double number2){
         return number1 + number2;
     }
 
+    /**
+     * Método para restar
+     * @param number1 primer operando
+     * @param number2 segundo operando
+     * @return el Resultado de la RESTA
+     */
     public double subtract(double number1, double number2){
         return number1 - number2;
     }
 
+    /**
+     * Método para multiplicar
+     * @param number1 primer operando
+     * @param number2 segundo operando
+     * @return el Resultado de la MULTIPLICACIÓN
+     */
     public double multiply(double number1, double number2){
         return number1 * number2;
     }
 
+    /**
+     * Método para dividir
+     * @param number1 prrimer operando
+     * @param number2 segundo operando
+     * @return el Resultado de la división
+     */
     public double split(double number1, double number2){
         return number1 / number2;
     }
--- src/main/java/com/sofka/test/Index.java
+++ src/main/java/com/sofka/test/Index.java
@@ -1,35 +1,42 @@
 package com.sofka.test;
 
 import com.sofka.domain.Calculadora;
-
 import javax.swing.*;
 
+/**
+ * Clase Index, donde inicia la ejecución
+ * @author Piter Velasquez & Álvaro Vega
+ * @version 25/05/2022
+ */
 public class Index {
 
+    /**
+     * Método main, punto de inicio de la aplicación
+     * @param args
+     */
     public static void main(String[] args) {
         int option;
-        double number1;
-        double number2;
+
         Calculadora calculadora = new Calculadora();
 
         do {
             option = Integer.parseInt(JOptionPane.showInputDialog("MENÚ\n\n\n1 -> Sumar\n2 -> Restar\n3 -> Multiplicar\n4 -> Dividir\n5 -> Salir\n\nDigite Opción:"));
 
             if (option !=5){
-                number1 = Double.parseDouble(JOptionPane.showInputDialog("Digite Número Uno:"));
-                number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite Número Dos:"));
+                calculadora.setNumber1(Double.parseDouble(JOptionPane.showInputDialog("Digite Número Uno:")));
+                calculadora.setNumber2(Double.parseDouble(JOptionPane.showInputDialog("Digite Número Dos:")));
                 switch (option) {
                     case 1:
-                        JOptionPane.showMessageDialog(null, "El Resultado de la SUMA es: " + calculadora.add(number1, number2));
+                        JOptionPane.showMessageDialog(null, "El Resultado de la SUMA es: " + calculadora.add(calculadora.getNumber1(), calculadora.getNumber2()));
                         break;
                     case 2:
-                        JOptionPane.showMessageDialog(null, "El Resultado de la RESTA es: " + calculadora.subtract(number1, number2));
+                        JOptionPane.showMessageDialog(null, "El Resultado de la RESTA es: " + calculadora.subtract(calculadora.getNumber1(), calculadora.getNumber2()));
                         break;
                     case 3:
-                        JOptionPane.showMessageDialog(null, "El Resultado de la MULTIPLICACIÓN es: " + calculadora.multiply(number1, number2));
+                        JOptionPane.showMessageDialog(null, "El Resultado de la MULTIPLICACIÓN es: " + calculadora.multiply(calculadora.getNumber1(), calculadora.getNumber2()));
                         break;
                     case 4:
-                        JOptionPane.showMessageDialog(null, "El Resultado de la DIVISIÓN es: " + calculadora.split(number1, number2));
+                        JOptionPane.showMessageDialog(null, "El Resultado de la DIVISIÓN es: " + calculadora.split(calculadora.getNumber1(), calculadora.getNumber2()));
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Opción Inválida");
